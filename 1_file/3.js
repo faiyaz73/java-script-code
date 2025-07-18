31db0193be6db06297492f432f2d59b1211a40a3
faiyaz ansari <131226634+faiyaz73@users.noreply.github.com>
2025-07-18 21:41:30 +0000

    data type

---
+++ 1_file/3.js
@@ -0,0 +1,18 @@
+// let score = "42"
+// console.log(typeof score)
+// console.log(typeof(score));
+// let num = Number(score)
+// console.log(typeof(num));
+
+
+
+// let num1 = 35
+// let num2 = "4"
+// let num3 = "87"
+// console.log(num1 + num2 + num3);
+
+// let num4 = Number(num2)
+// let num5 = Number(num3)
+// console.log(num1 + num4 + num5)
+let num = (2 + "3" + 2)
+console.log(typeof num,num)
