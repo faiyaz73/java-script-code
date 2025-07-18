7b436b1d6e016cf0cd6335f055e79d8742ff51b7
faiyaz ansari <131226634+faiyaz73@users.noreply.github.com>
2025-07-18 20:51:58 +0000

    data type

---
+++ 1_file/2.js
@@ -0,0 +1,15 @@
+// data type 
+ 
+// number => 2 to 53
+// BigInt 
+// String
+// Boolean
+// null standalone  value 
+// undefined
+// Symbol => unique
+
+
+
+// object 
+let name = "faiyaz"
+console.log(typeof null)
